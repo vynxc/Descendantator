86747241898345d930d2ef601e9147fe48224eda
vynxc <svynxc@gmail.com>
2024-10-21 08:07:45 -0700

    Add Player struct and buffer management to project
    Included Player.cpp and Player.hpp in Descendantator.vcxproj.
    Updated .vcxproj.filters to categorize new files correctly.
    Defined Player struct and buffer management in Player.hpp:
    - Added buffers, active buffer, and player count.
    - Declared ResetPlayerBuffers function.
    Implemented Player.cpp with buffer initialization and reset logic.

--- Descendantator/Player.hpp
+++ Descendantator/Player.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <array>
 #include "SDK/Engine_classes.hpp"
 
 struct Player {
@@ -7,3 +8,10 @@ struct Player {
     SDK::FVector2D Health;
     SDK::FVector Rotation;
 };
+
+constexpr size_t MAX_PLAYERS = 100;
+extern std::array<Player, MAX_PLAYERS> buffer1, buffer2;
+extern std::atomic<std::array<Player, MAX_PLAYERS>*> active_buffer;
+extern std::atomic<size_t> player_count;
+
+void ResetPlayerBuffers();
